051bc5d732b7917fb21d708144b1e0a26abd08ea
Caroline Ta <qdttdev@gmail.com>
2021-06-04 16:03:28 -0700

    Create Best_Time_to_Buy_and_Sell_Stock_I|.cpp

---
+++ Top Interview Questions/Easy/Best_Time_to_Buy_and_Sell_Stock_I|.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        int maxProf = 0;
+        
+        // Traverse through the array until the second last element
+        // to make sure prices[j] will not be out of bound
+        for(int i = 0, j = 1; i < prices.size()-1; i++, j++)
+        {
+            // We only profit when prices sold is higher than prices bought
+            // and we have to buy in the day before we sell it: pricesBuy < pricesSell
+            if(prices[i] < prices[j])
+            {
+                maxProf += prices[j] - prices[i];
+            }
+        }
+        
+        return maxProf;        
+    }
+};
