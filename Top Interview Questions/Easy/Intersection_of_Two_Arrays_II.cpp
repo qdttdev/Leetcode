1d623f5da9037f2e2e58fea75583f5cc7a7bdccd
Caroline Ta <qdttdev@gmail.com>
2021-06-07 19:51:19 -0700

    Create Intersection_of_Two_Arrays_II.cpp

---
+++ Top Interview Questions/Easy/Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
+// What's the time complexity? - O(n*m), n = smallerSize, m = biggerSize
+// What's the space complexity? - O(n), n = the numbers in the intersection
+
+class Solution {
+public: 
+    
+    vector<int> smallerIntersection(vector<int>& smaller, vector<int>& bigger)
+    {
+        vector<int> interV;
+        int j = 0;
+
+        // Use smaller as base case to traverse to find intersections
+        for(int i = 0; i < smaller.size(); i++)
+        {
+            // Traverse through bigger to find matches
+            while(j < bigger.size())
+            {
+                // If match is found, erase it from bigger to prevent duplicates
+                // Push back into the returning array interV
+                // Break because we don't need to compare with the rest of bigger
+                if(smaller[i] == bigger[j])
+                {
+                    bigger.erase(bigger.begin()+j);
+                    interV.push_back(smaller[i]);
+                    break;
+                }
+                else // Increment to see if match is found in next element in bigger
+                {
+                    j++;
+                }
+            }
+            
+            // Reset j for the next traversal through j for new i
+            j = 0;
+        }
+        
+        return interV;        
+    }
+    
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) 
+    {
+        // It makes sense to use the smaller-size array to check for intersections       
+        if(nums1.size() <= nums2.size())
+        {
+            return smallerIntersection(nums1, nums2);
+        }
+        else
+        {
+            return smallerIntersection(nums2, nums1);
+        }
+    }
+};
