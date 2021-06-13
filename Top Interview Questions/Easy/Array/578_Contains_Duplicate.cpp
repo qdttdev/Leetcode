2c6dc0fb2a01d44447aa8000bdcf74652d248cc0
Caroline Ta <qdttdev@gmail.com>
2021-06-13 12:38:19 -0700

    Rename, add description and approach

--- Top Interview Questions/Easy/Array/578_Contains_Duplicate.cpp
+++ Top Interview Questions/Easy/Array/578_Contains_Duplicate.cpp
@@ -1,4 +1,11 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+// Time complexity: O(n), n = size of nums
+// Space complexity: O(n), n = size of nums
+
+// Description: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+// Approach: Use map to count, and output if distinct based on count.
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) 
