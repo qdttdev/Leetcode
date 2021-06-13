9a089713524a2bd15d537d5abda4257161e04d57
Caroline Ta <qdttdev@gmail.com>
2021-06-13 12:52:20 -0700

    Rename, add ques and appr

--- Top Interview Questions/Easy/Array/646_Rotate_Array.cpp
+++ Top Interview Questions/Easy/Array/646_Rotate_Array.cpp
@@ -1,4 +1,17 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+// Time complexity: O(n), n = size of nums
+// Space complexity: O(n), n = size of nums
+
+// Question: Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+// Approach: After k rotations, the numbers will be in their beginning positions so we use mod to
+// minimize the rotations. j = k % nums.size()
+
+// Push the rotated numbers into the returning vector, then push the rest.
+// Why is i = nums.size()-j ?
+// Recognize that rotating 3 steps mean the last 3 elements are totated to the front,
+// so the start index i will start at the 3rd to last element then go to last element in the array.
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) 
