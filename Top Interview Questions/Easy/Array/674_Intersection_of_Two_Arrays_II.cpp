e109adcc72f0541fa78aaabc5bb5a0bbda6c1c27
Caroline Ta <qdttdev@gmail.com>
2021-06-13 12:29:20 -0700

    Rename, add description, approach

--- Top Interview Questions/Easy/Array/674_Intersection_of_Two_Arrays_II.cpp
+++ Top Interview Questions/Easy/Array/674_Intersection_of_Two_Arrays_II.cpp
@@ -2,6 +2,14 @@
 // What's the time complexity? - O(n*m), n = smallerSize, m = biggerSize
 // What's the space complexity? - O(n), n = the numbers in the intersection
 
+// Question: Given two integer arrays nums1 and nums2, return an array of their intersection. 
+// Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Approach: We use the smaller-size array to check for intersections.
+// Use two loops, smaller-size array as outer, bigger-size array as inner.
+// Compare element of smaller == bigger, if exist, then erase it from bigger to prevent duplicates,
+// and push that element in the returning vector.
+
 class Solution {
 public: 
     
