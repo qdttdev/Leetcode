a58a03adcd4a71aadc782307de45660bd7b7f8f4
Caroline Ta <qdttdev@gmail.com>
2021-06-13 12:56:39 -0700

    Rename, add ques and appr

--- Top Interview Questions/Easy/Array/564_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ Top Interview Questions/Easy/Array/564_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -1,4 +1,13 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+// Time complexity: O(n), n = size of prices
+// Space complexity: O(1)
+
+// Question: You are given an array prices where prices[i] is the price of a given stock on the ith day.
+// Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+// Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+// Approach: Compare two elements that are next to each other, since we buy before we sell, if buy is lower than sell, then profits are made.
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) 
