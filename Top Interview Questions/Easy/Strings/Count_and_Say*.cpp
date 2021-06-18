3c853c71f901bf76f53a46befa3b719f6934b8db
Caroline Ta <qdttdev@gmail.com>
2021-06-17 23:18:31 -0700

    Create Count_and_Say*.cpp

---
+++ Top Interview Questions/Easy/Strings/Count_and_Say*.cpp
@@ -0,0 +1,54 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/886/
+
+// Question: Implement countAndSay, 
+//  1.     1                    one 1                   11
+//  2.     11                   two 1                   21
+//  3.     21                   one 2, one 1            1211
+//  4.     1211                 one 1, one 2, two 1     111221
+//  5.     111221               ...
+//  6.     312211
+//  7.     13112221
+//  8.     1113213211
+//  9.     31131211131221
+// 10.     13211311123113112211
+
+
+class Solution {
+public:
+    
+    string recur(string s)
+    {
+        int count = 0;
+        string result = "";
+        
+        for(int i = 0; i < s.size(); i++)
+        {
+            // This is the first occurence of s[i]
+            count++;
+            
+            // If the next element isn't the same, then we can put "count" into the result
+            if(s[i] != s[i+1])
+            {
+                // This says, for example: two 1's, result will be 21
+                result = result + char('0' + count) + s[i];
+                
+                // Reset count for next count for a different element
+                count = 0;
+            }            
+        }
+        
+        return result;
+    }
+    
+    string countAndSay(int n) 
+    {
+        // Base case: n = 1
+        if(n == 1)
+        {
+            return "1";
+        }
+        
+        // To generate the nth term, just countAndSay the (n-1)th term
+        return recur(countAndSay(n-1));            
+    }
+};
